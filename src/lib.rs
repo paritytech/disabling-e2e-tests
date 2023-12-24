650c3e6988a99667727f525cd8582d22ae473fd9
ordian <noreply@reusable.software>
2023-12-24 15:12:17 +0100

    test offchain disputes disabling

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,9 @@ use zombienet_sdk::{
 #[cfg(test)]
 mod tests;
 
+#[subxt::subxt(runtime_metadata_path = "artifacts/polkadot_metadata_small.scale")]
+pub mod polkadot {}
+
 pub type Error = Box<dyn std::error::Error>;
 
 pub fn runtime_config() -> serde_json::Value {
@@ -30,7 +33,7 @@ pub fn runtime_config() -> serde_json::Value {
 ///
 /// This spawns a network with 3 honest validators and 1 malicious backer.
 /// One parachain with id 2000 and one cumulus-based collator.
-pub async fn spawn_network() -> Result<Network<LocalFileSystem>, Error> {
+pub async fn spawn_network_malus_backer() -> Result<Network<LocalFileSystem>, Error> {
     let network = NetworkConfigBuilder::new()
         .with_relaychain(|r| {
             let patch = runtime_config();
@@ -49,6 +52,49 @@ pub async fn spawn_network() -> Result<Network<LocalFileSystem>, Error> {
                     node.with_name("malicious-backer")
                         .with_command("malus")
                         .with_subcommand("suggest-garbage-candidate")
+                        .with_args(vec![
+                            "--insecure-validator-i-know-what-i-do".into(),
+                            "-lMALUS=trace".into(),
+                        ])
+                })
+        })
+        .with_parachain(|p| {
+            p.with_id(2000)
+                .cumulus_based(true)
+                .with_registration_strategy(RegistrationStrategy::InGenesis)
+                .with_collator(|n| n.with_name("collator").with_command("polkadot-parachain"))
+        })
+        .build()
+        .unwrap()
+        .spawn_native()
+        .await?;
+
+    Ok(network)
+}
+
+// FIXME: deduplicate this
+pub async fn spawn_network_dispute_valid() -> Result<Network<LocalFileSystem>, Error> {
+    let network = NetworkConfigBuilder::new()
+        .with_relaychain(|r| {
+            let patch = runtime_config();
+
+            r.with_chain("westend-local")
+                .with_genesis_overrides(patch)
+                .with_default_command("polkadot")
+                .with_default_args(vec![
+                    "--insecure-validator-i-know-what-i-do".into(),
+                    "-lparachain=debug".into(),
+                ])
+                .with_node(|node| node.with_name("honest-0"))
+                .with_node(|node| node.with_name("honest-1"))
+                .with_node(|node| node.with_name("honest-2"))
+                .with_node(|node| node.with_name("honest-3"))
+                .with_node(|node| node.with_name("honest-4"))
+                .with_node(|node| node.with_name("honest-5"))
+                .with_node(|node| {
+                    node.with_name("malicious-disputer")
+                        .with_command("malus")
+                        .with_subcommand("dispute-ancestor")
                         .with_args(vec![
                             "--insecure-validator-i-know-what-i-do".into(),
                             "-lMALUS=trace".into(),
--- src/tests.rs
+++ src/tests.rs
@@ -1,10 +1,12 @@
 use crate::*;
 
+const DISPUTES_TOTAL_METRIC: &str = "polkadot_parachain_candidate_disputes_total";
+
 #[tokio::test]
-async fn simple() -> Result<(), Error> {
+async fn test_backing_disabling() -> Result<(), Error> {
     tracing_subscriber::fmt::init();
 
-    let network = spawn_network().await?;
+    let network = spawn_network_malus_backer().await?;
 
     println!("🚀🚀🚀 network deployed");
 
@@ -16,9 +18,77 @@ async fn simple() -> Result<(), Error> {
 
     wait_for_block(1, collator_client).await?;
 
-    wait_for_metric(honest, "polkadot_parachain_candidate_disputes_total", 1).await?;
+    wait_for_metric(honest, DISPUTES_TOTAL_METRIC, 1).await?;
 
-    // let honest_client = honest.client::<subxt::PolkadotConfig>().await?;
+    let honest_client = honest.client::<subxt::PolkadotConfig>().await?;
+
+    // wait until we have the malicious validator disabled
+    loop {
+        let call = polkadot::apis().parachain_host().disabled_validators();
+        let disabled = honest_client
+            .runtime_api()
+            .at_latest()
+            .await?
+            .call(call)
+            .await?;
+        if disabled.len() == 1 {
+            break;
+        }
+        sleep(Duration::from_secs(5)).await;
+    }
+
+    // NOTE: there's a race condition possible
+    // after the validator got disabled, but disputes are still ongoing
+    // wait for a couple of blocks to avoid it
+    sleep(Duration::from_secs(12)).await;
+
+    // get the current disputes metric
+    let total_disputes = honest.reports(DISPUTES_TOTAL_METRIC).await? as u64;
+
+    // wait a bit
+    sleep(Duration::from_secs(120)).await;
+
+    let new_total_disputes = honest.reports(DISPUTES_TOTAL_METRIC).await? as u64;
+
+    // ensure that no new disputes were created after validator got disabled
+    assert_eq!(total_disputes, new_total_disputes);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_disputes_offchain_disabling() -> Result<(), Error> {
+    tracing_subscriber::fmt::init();
+
+    let network = spawn_network_dispute_valid().await?;
+
+    println!("🚀🚀🚀 network deployed");
+
+    let honest = network.get_node("honest-0")?;
+    let role = honest.reports("node_roles").await?;
+    assert_eq!(role as u64, 4);
+
+    let collator_client = get_client(&network, "collator").await?;
+
+    wait_for_block(1, collator_client).await?;
+
+    wait_for_metric(honest, DISPUTES_TOTAL_METRIC, 1).await?;
+
+    // NOTE: there's a race condition possible
+    // after the dispute concluded and before the validator got disabled
+    // wait for a block to avoid it
+    sleep(Duration::from_secs(6)).await;
+
+    // get the current disputes metric
+    let total_disputes = honest.reports(DISPUTES_TOTAL_METRIC).await? as u64;
+
+    // wait a bit
+    sleep(Duration::from_secs(60)).await;
+
+    let new_total_disputes = honest.reports(DISPUTES_TOTAL_METRIC).await? as u64;
+
+    // ensure that no new disputes were created after validator got disabled offchain
+    assert_eq!(total_disputes, new_total_disputes);
 
     Ok(())
 }
